c98f615f83855525bb904733134bf51f67a3a44d
malken21 <77374813+malken21@users.noreply.github.com>
2024-07-13 21:01:08 +0900

    Update MinumaWindow.java

--- src/main/java/marumasa/minuma_window/MinumaWindow.java
+++ src/main/java/marumasa/minuma_window/MinumaWindow.java
@@ -1,20 +1,24 @@
 package marumasa.minuma_window;
 
+import com.mojang.logging.LogUtils;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
 
 public class MinumaWindow implements ModInitializer {
-
+    // Logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+    // Mod ID
     public static final String MOD_ID = "minuma_window";
-
     // サウンドイベント登録
     public static final SoundEvent MinumaIku_Sound = registerSound("minuma.iku");
 
     @Override
     public void onInitialize() {
+        LOGGER.info(String.format("Start: %s", MOD_ID));
     }
 
     // サウンドイベント登録メソッド
